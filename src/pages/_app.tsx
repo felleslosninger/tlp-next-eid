e41d5f4e749834c19211bb72df99b0d9d74efff6
Jens Mo <97087448+jensmo98@users.noreply.github.com>
2023-05-05 12:12:06 +0200

    Installed and implemented normalize.css (#2)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import '@/globals.css';
 import type { AppProps } from 'next/app';
+import 'normalize.css';
 
 import { initializeLocalization } from './../i18n';
 
 initializeLocalization();
-
-//Import i18n.ts
-//import './../i18n';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
