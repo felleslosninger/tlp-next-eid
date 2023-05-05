6a24b51d6118507dabf8fb7cfe27664ad2e57bfa
Jens Mo <jens19mo98@gmail.com>
2023-05-05 08:25:12 +0200

    Tested to translate in the footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 import classes from './Footer.module.css';
 
@@ -7,9 +8,11 @@ interface FooterProps {
 }
 
 const Footer = ({ text }: FooterProps) => {
+  const { t } = useTranslation(['main']);
   return (
     <footer className={classes.footer}>
       <div>Footer {text}</div>
+      {t('tomat')}
     </footer>
   );
 };
