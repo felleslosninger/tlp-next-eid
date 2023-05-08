213921dcb0585ad8eab41c121a60a215e6f10c9e
Jens Mo <jens19mo98@gmail.com>
2023-05-08 08:08:47 +0200

    Created dummy header, to test store. created interface for active page footer and header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+import classes from './Header.module.css';
+
+interface HeaderProps {
+  title?: string;
+  logo?: { alt: string; url: string };
+  items?: [React.ReactNode];
+}
+
+const Header = ({ title }: HeaderProps) => {
+  const { t } = useTranslation(['main']);
+  return (
+    <header className={classes.header}>
+      <div>{title}</div>
+      {t('tomat')}
+    </header>
+  );
+};
+
+export type { HeaderProps };
+export { Header };
