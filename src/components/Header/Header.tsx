ef84bdc6b535375e9d2952b81934ff0b0c69068a
Jens Mo <jens19mo98@gmail.com>
2023-05-10 16:03:02 +0200

    Created Container component, folder stucture for layout components and created the Layout component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
+//import { useTranslation } from 'react-i18next';
+
+import { Container } from '../Container.tsx/Container';
+import { useAppSelector } from '../../store/hooks';
 
 import classes from './Header.module.css';
+import { changeLanguage } from './../../i18n';
 
 interface HeaderProps {
   title?: string;
@@ -10,11 +14,30 @@ interface HeaderProps {
 }
 
 const Header = ({ title }: HeaderProps) => {
-  const { t } = useTranslation(['main']);
+  const activePage = useAppSelector((state) => state.activePage);
+  const onClickLanguageChange = (
+    e:
+      | React.ChangeEvent<HTMLSelectElement>
+      | React.MouseEvent<HTMLButtonElement>,
+  ) => {
+    if (typeof e.currentTarget.value === 'string') {
+      changeLanguage(e.currentTarget.value);
+    }
+  };
   return (
     <header className={classes.header}>
-      <div className={classes.title}>{title}</div>
-      {t('tomat')}
+      <Container>
+        <h1 className={classes.title}>{activePage.content.node.title}</h1>
+        <select
+          className='custom-select'
+          style={{ width: 200 }}
+          onChange={onClickLanguageChange}
+        >
+          <option value='nb'>Norsk</option>
+          <option value='en'>English</option>
+          <option value='sa'>sámegiella</option>
+        </select>
+      </Container>
     </header>
   );
 };
