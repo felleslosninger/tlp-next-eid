81da18a00aa6336e1119d0bb57afd9756de2eb44
Øyvind Thune <oyvlei@gmail.com>
2023-05-16 11:55:44 +0200

    More stuff

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
-
-import { Layout } from '@/layout/Layout';
-
-import IndexFile from '../IndexFile';
-
 export default function Page({ children }) {
   return (
-    <IndexFile>
-      <Layout>{children}</Layout>
-    </IndexFile>
+    <div>
+      <div>{children}</div>
+    </div>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,12 +5,18 @@ import 'normalize.css';
 import '@altinn/figma-design-tokens/dist/tokens.css';
 import '@digdir/design-system-tokens/dist/digdir/tokens.css';
 import '@digdir/tlp-react/dist/tokens.css';
+import { Layout } from '@/layout/Layout';
 
-interface IndexProps {
+export default function RootLayout({
+  children,
+}: {
   children: React.ReactNode;
+}) {
+  return (
+    <html lang='en'>
+      <body className={InterFont.className}>
+        <Layout>{children}</Layout>
+      </body>
+    </html>
+  );
 }
-const IndexFile = ({ children }: IndexProps) => {
-  return <div className={InterFont.className}>{children}</div>;
-};
-
-export default IndexFile;
