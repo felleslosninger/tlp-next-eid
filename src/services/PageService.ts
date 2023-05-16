30b2b9f22cb9243033bf9c2b2846c75377ee176e
Jens Mo <jens19mo98@gmail.com>
2023-05-16 09:52:47 +0200

    Rendering from api

--- src/services/PageService.ts
+++ src/services/PageService.ts
@@ -1,6 +1,12 @@
-const GetPage = async () => {
+import { buildUrl } from '@/utils/buildUrl';
+const GetPage = async (params: any) => {
+  const path = buildUrl(params);
+  console.log(path);
   const response = await fetch(
-    'http://feat01-drupal8.dmz.local/eid/nb/api/rest/page?lang=nb&path=/id-porten',
+    'http://feat01-drupal8.dmz.local/eid/nb/api/rest/page?lang=' +
+      params.lang +
+      '&path=/' +
+      path,
   );
   return response.json();
 };
