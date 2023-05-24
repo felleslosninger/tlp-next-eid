27d91fec3d1c4c5c970e155132d0d85fe982014a
Jens Mo <jens19mo98@gmail.com>
2023-05-24 13:10:27 +0200

    bug fixes in i18n

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import useTranslation from 'next-translate/useTranslation';
 
 import { GetPage } from '@/services/PageService';
 import type { ApiDataType, ParamsType } from '@/types/ApiData';
 
 export default async function Page(params: ParamsType) {
+  const { t } = useTranslation();
   const data: ApiDataType = await GetPage(params);
 
   return (
     <>
       <h1>{data.content.node.title}</h1>
       <p>{data.content.node.body}</p>
+      <h1>{t`tomat`}</h1>
     </>
   );
 }
