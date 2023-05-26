aef92e393798202b0ca3701b1e317d45e88ed999
Jens Mo <jens19mo98@gmail.com>
2023-05-26 14:11:39 +0200

    Fixed typerrors in get-dictionary

--- src/get-dictionary.ts
+++ src/get-dictionary.ts
@@ -1,12 +1,13 @@
-//import 'server-only';
 import type { Locale } from './i18n-config';
 
-// We enumerate all dictionaries here for better linting and typescript support
-// We also get the default import for cleaner types
-const dictionaries = {
-  en: () => import('./dictionaries/en.json').then((module) => module.default),
+interface Dictionary {
+  [key: string]: () => Promise<{ [key: string]: string }>;
+}
+
+const dictionaries: Dictionary = {
   nb: () => import('./dictionaries/nb.json').then((module) => module.default),
   se: () => import('./dictionaries/se.json').then((module) => module.default),
+  en: () => import('./dictionaries/en.json').then((module) => module.default),
 };
 
 export const getDictionary = async (locale: Locale) => dictionaries[locale]();
