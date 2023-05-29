32b123dfcb20be4d1ac5ed99202894a77204e3f1
Jens Mo <97087448+jensmo98@users.noreply.github.com>
2023-05-29 12:41:12 +0200

    moved layout file into [lang] folder

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -2,16 +2,12 @@ import React from 'react';
 
 import { InterFont } from '@/fonts/Interfont';
 
-import { i18n } from '../i18n-config';
+//import { i18n } from '../i18n-config';
 import 'normalize.css';
 import '@altinn/figma-design-tokens/dist/tokens.css';
 import '@digdir/design-system-tokens/dist/digdir/tokens.css';
 import '@digdir/tlp-react/dist/tokens.css';
-import '../globals.css';
-
-export function generateStaticParams(): Promise<{ lang: string }[]> {
-  return Promise.resolve(i18n.locales.map((locale) => ({ lang: locale })));
-}
+import '../../globals.css';
 
 export default function Root({
   children,
--- src/app/[lang]/search/page.tsx
+++ src/app/[lang]/search/page.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
 
-import { GetPage } from '@/services/PageService';
-import type { ApiDataType, ParamsType } from '@/types/ApiData';
+import type { ParamsType } from '@/types/ApiData';
 import { Layout } from '@/layout/Layout';
 
 import { getDictionary } from '../../../get-dictionary';
 
 export default async function IndexPage({ params }: ParamsType) {
   const dictionary = await getDictionary(params.lang);
-  const data: ApiDataType = await GetPage(params.lang, params.slug);
 
   return (
     <Layout dictionary={dictionary}>
-      <h1>{data.content.node.title}</h1>
-      <p>{data.content.node.body}</p>
+      <h1>Search</h1>
     </Layout>
   );
 }
